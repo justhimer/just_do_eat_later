77dc36ca3423b83bda5c3e47b1d7544658c9cc3a
Geoffrey Luk <luk.geoffreycw@gmail.com>
2023-02-23 10:45:50 +0800

    login update

---
+++ util/middleware.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import expressSession from 'express-session'
+import http from "http"
+import { Server as SocketIO } from "socket.io"
+import dotenv from "dotenv"
+import grant from 'grant'
+
+export const sessionMiddleware = expressSession({
+    secret: "workout and eat",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {secure:false}
+})
+
+export const app = express();
+export const server = new http.Server(app)
+export const io = new SocketIO(server)
+
+//google oauth
+dotenv.config()
+export const grantExpress = grant.express({
+    defaults: {
+        origin: "http://localhost:8080",
+        transport: "session",
+        state: "true"
+    },
+    google:{
+        key: process.env.GOOGLE_CLIENT_ID || "",
+        secret: process.env.GOOGLE_CLIENT_SECRET || "",
+        scope: ["profile","email"],
+        callback: "/users/login/google"
+    }
+})
