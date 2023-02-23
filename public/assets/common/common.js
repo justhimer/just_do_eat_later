886a030b74ff0d9f847c9a41cadeb0c4f46c0dc0
Geoffrey Luk <luk.geoffreycw@gmail.com>
2023-02-23 19:00:03 +0800

    finished nav bar

--- public/assets/common/common.js
+++ public/assets/common/common.js
@@ -2,24 +2,21 @@ const menu = document.querySelector("#hamburger")
 const loginStatus = getStatus
 
 window.onload = async () => {
-    // menuLoader()
-    // constRefresh()
-}
+    
+    let elem,
+        toggleBtn,
+        elemH,
+        elemW;
 
-let elem = document.querySelector('#nav-bg'),
-    toggleBtn = document.querySelector('#toggle-btn'),
-    elemH = elem.getBoundingClientRect().height,
-    elemW = elem.getBoundingClientRect().width;
+    await menuLoader()
+    await refresh()
 
-
-let open = false;
+    let open = false;
 let scale, offsetX, offsetY;
 
 const calculateValues = (() => {
     const w = window.innerWidth;
     const h = window.innerHeight;
-    //const cssStyles = getComputedStyle(elem);
-    //const offsetValue = Number(cssStyles.getPropertyValue('--offset-value'));
     const offsetValue = Number(getComputedStyle(elem).getPropertyValue('--offset-value'));
 
     //  Offsets to center the circle
@@ -63,12 +60,24 @@ const resizeHandler = () => {
         animateMenu();
     });
 }
+    calculateValues();
 
-calculateValues();
 
-//toggleBtn.onclick = toggleMenu;
-toggleBtn.addEventListener('click', toggleMenu, false);
-window.addEventListener("resize", resizeHandler, false);
+    window.addEventListener("resize", resizeHandler, false);
+
+    menu.addEventListener('click',(event)=>{
+        if (event.target == document.querySelector('#toggle-btn')){
+            toggleMenu()
+        }
+    })
+
+    async function refresh(){
+        elem = document.querySelector('#nav-bg'),
+        toggleBtn = document.querySelector('#toggle-btn'),
+        elemH = elem.getBoundingClientRect().height,
+        elemW = elem.getBoundingClientRect().width;
+    }
+}
 
 async function getStatus() {
     const res = await fetch('/users/loginStatus')
@@ -79,7 +88,6 @@ async function getStatus() {
         return false
     }
 }
-
 
 async function menuLoader() {
     const status = await loginStatus()
@@ -92,7 +100,7 @@ async function menuLoader() {
         <span></span>
     </div>
     `
-    if (!status) {
+    if (status) {
         menu.innerHTML += `
         <div class="wrapper">
                 <nav class="nav_container">
@@ -137,79 +145,4 @@ async function menuLoader() {
             </div>
         `
     }
-    constRefresh()
-
-}
-
-async function constRefresh(){
-    elem = document.querySelector('#nav-bg'),
-    toggleBtn = document.querySelector('#toggle-btn'),
-    elemH = elem.getBoundingClientRect().height,
-    elemW = elem.getBoundingClientRect().width;
-}
-
-function remainder(){
-    let elem = document.querySelector('#nav-bg'),
-    toggleBtn = document.querySelector('#toggle-btn'),
-    elemH = elem.getBoundingClientRect().height,
-    elemW = elem.getBoundingClientRect().width;
-
-
-let open = false;
-let scale, offsetX, offsetY;
-
-const calculateValues = (() => {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    //const cssStyles = getComputedStyle(elem);
-    //const offsetValue = Number(cssStyles.getPropertyValue('--offset-value'));
-    const offsetValue = Number(getComputedStyle(elem).getPropertyValue('--offset-value'));
-
-    //  Offsets to center the circle
-    offsetX = (w / 2) - (elemW / 2) - offsetValue - 10;
-    offsetY = (h / 2) - (elemH / 2) - offsetValue - 10;
-
-    // Good old pythagoras
-    const radius = Math.sqrt((h ** 2) + (w ** 2));
-    scale = radius / (elemW / 2) / 2 + .1; // Add '.1' to compensate for Safari sub pixel blur issue
-    return scale;
-})
-
-
-const openMenu = () => {
-    elem.style.setProperty("--translate-x", `${offsetX}px`);
-    elem.style.setProperty("--translate-y", `-${offsetY}px`);
-    elem.style.setProperty("--scale", scale);
-    elem.style.setProperty("background-color", "#3B3836");
-}
-const closeMenu = () => {
-    elem.style.setProperty("--scale", 1);
-    elem.style.setProperty("--translate-x", 0);
-    elem.style.setProperty("--translate-y", 0);
-    setTimeout(() => { elem.style["background-color"] = null; }, 300)
-
-}
-const animateMenu = () => {
-    open ? openMenu() : closeMenu();
-};
-
-const toggleMenu = () => {
-    open = !open;
-    animateMenu();
-    toggleBtn.classList.toggle('shown');
-
-}
-
-const resizeHandler = () => {
-    window.requestAnimationFrame(() => {
-        calculateValues();
-        animateMenu();
-    });
-}
-
-calculateValues();
-
-//toggleBtn.onclick = toggleMenu;
-toggleBtn.addEventListener('click', toggleMenu, false);
-window.addEventListener("resize", resizeHandler, false);
 }
