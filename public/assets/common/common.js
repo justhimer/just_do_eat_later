9ed52ceca4b8ecc523ed9a11c166129ac27d3dcf
Geoffrey Luk <luk.geoffreycw@gmail.com>
2023-02-24 01:17:33 +0800

    further edits to nav bar

--- public/assets/common/common.js
+++ public/assets/common/common.js
@@ -2,80 +2,95 @@ const menu = document.querySelector("#hamburger")
 const loginStatus = getStatus
 
 window.onload = async () => {
-    
+    //init important variables
+    /* #region session */
     let elem,
         toggleBtn,
         elemH,
-        elemW;
+        elemW,
+        open = false,
+        scale,
+        offsetX,
+        offsetY;
+    /* #endregion */
 
+    //inject menu into html & re-init variables to document.queryselector
+    /* #region session */
     await menuLoader()
     await refresh()
 
-    let open = false;
-let scale, offsetX, offsetY;
+    /* #endregion */
 
-const calculateValues = (() => {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    const offsetValue = Number(getComputedStyle(elem).getPropertyValue('--offset-value'));
+    //core math logic for animation
+    /* #region session */
+    const calculateValues = (() => {
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+        const offsetValue = Number(getComputedStyle(elem).getPropertyValue('--offset-value'));
 
-    //  Offsets to center the circle
-    offsetX = (w / 2) - (elemW / 2) - offsetValue - 10;
-    offsetY = (h / 2) - (elemH / 2) - offsetValue - 10;
+        //  Offsets to center the circle
+        offsetX = (w / 2) - (elemW / 2) - offsetValue;
+        offsetY = (h / 2) - (elemH / 2) - offsetValue;
 
-    // Good old pythagoras
-    const radius = Math.sqrt((h ** 2) + (w ** 2));
-    scale = radius / (elemW / 2) / 2 + .1; // Add '.1' to compensate for Safari sub pixel blur issue
-    return scale;
-})
+        // Good old pythagoras
+        const radius = Math.sqrt((h ** 2) + (w ** 2));
+        scale = radius / (elemW / 2) / 2 + .1; // Add '.1' to compensate for Safari sub pixel blur issue
+        return scale;
+    })
+    /* #endregion */
 
+    //logic controlling animation
+    /* #region session */
+    const openMenu = () => {
+        elem.style.setProperty("--translate-x", `${offsetX}px`);
+        elem.style.setProperty("--translate-y", `-${offsetY}px`);
+        elem.style.setProperty("--scale", scale);
+        elem.style.setProperty("background-color", "#3B3836");
+    }
+    const closeMenu = () => {
+        elem.style.setProperty("--scale", 1);
+        elem.style.setProperty("--translate-x", 0);
+        elem.style.setProperty("--translate-y", 0);
+        setTimeout(() => { elem.style["background-color"] = null; }, 300)
 
-const openMenu = () => {
-    elem.style.setProperty("--translate-x", `${offsetX}px`);
-    elem.style.setProperty("--translate-y", `-${offsetY}px`);
-    elem.style.setProperty("--scale", scale);
-    elem.style.setProperty("background-color", "#3B3836");
-}
-const closeMenu = () => {
-    elem.style.setProperty("--scale", 1);
-    elem.style.setProperty("--translate-x", 0);
-    elem.style.setProperty("--translate-y", 0);
-    setTimeout(() => { elem.style["background-color"] = null; }, 300)
+    }
+    const animateMenu = () => {
+        open ? openMenu() : closeMenu();
+    };
 
-}
-const animateMenu = () => {
-    open ? openMenu() : closeMenu();
-};
-
-const toggleMenu = () => {
-    open = !open;
-    animateMenu();
-    toggleBtn.classList.toggle('shown');
-
-}
-
-const resizeHandler = () => {
-    window.requestAnimationFrame(() => {
-        calculateValues();
+    const toggleMenu = () => {
+        open = !open;
         animateMenu();
-    });
-}
-    calculateValues();
+        toggleBtn.classList.toggle('shown');
 
+    }
 
+    const resizeHandler = () => {
+        window.requestAnimationFrame(() => {
+            calculateValues();
+            animateMenu();
+        });
+    }
+    /* #endregion */
+
+    calculateValues(); // don't move
+
+    //control click and resize events
+    /* #region session */
     window.addEventListener("resize", resizeHandler, false);
-
-    menu.addEventListener('click',(event)=>{
-        if (event.target == document.querySelector('#toggle-btn')){
+    menu.addEventListener('click', (event) => {
+        if (event.target == document.querySelector('#toggle-btn')) {
             toggleMenu()
         }
     })
+    /* #endregion */
 
-    async function refresh(){
+
+    async function refresh() { // don't move outside onload, re-init variables
         elem = document.querySelector('#nav-bg'),
-        toggleBtn = document.querySelector('#toggle-btn'),
-        elemH = elem.getBoundingClientRect().height,
-        elemW = elem.getBoundingClientRect().width;
+            toggleBtn = document.querySelector('#toggle-btn'),
+            elemH = elem.getBoundingClientRect().height,
+            elemW = elem.getBoundingClientRect().width;
     }
 }
 
@@ -105,20 +120,20 @@ async function menuLoader() {
         <div class="wrapper">
                 <nav class="nav_container">
                     <ul class="nav_ul">
-                        <li><a class="nav link" href="#0">Projects</a></li>
-                        <li><a class="nav link" href="#0">About</a></li>
-                        <li><a class="nav link" href="#0">Contact</a></li>
+                        <li><a class="nav link" href="/">Home</a></li>
+                        <li><a class="nav link" href="#0">Just Do</a></li>
+                        <li><a class="nav link" href="#0">Eat Later</a></li>
                     </ul>
                     <div class="other_container">
-                        <a href="*"><div class="icon_container">
+                        <a href="*"><div class="icon_container pulse circle">
                             <span class="material-symbols-outlined">account_circle</span>
                             <p>Profile</p>
                         </div></a>
-                        <a href="/users/logout"><div class="icon_container">
+                        <a href="/users/logout"><div class="icon_container pulse circle">
                             <span class="material-symbols-outlined">logout</span>
                             <p>Logout</p>
                         </div></a>
-                        <a href="*"><div class="icon_container">
+                        <a href="*"><div class="icon_container pulse circle">
                             <span class="material-symbols-outlined">shopping_cart</span>
                             <p>Cart</p>
                         </div></a>
@@ -131,12 +146,12 @@ async function menuLoader() {
         <div class="wrapper">
                 <nav class="nav_container">
                     <ul class="nav_ul">
-                        <li><a class="nav link" href="#0">Projects</a></li>
-                        <li><a class="nav link" href="#0">About</a></li>
-                        <li><a class="nav link" href="#0">Contact</a></li>
+                        <li><a class="nav link" href="/">Home</a></li>
+                        <li><a class="nav link" href="#0">Just Do</a></li>
+                        <li><a class="nav link" href="#0">Eat Later</a></li>
                     </ul>
                     <div class="other_container">
-                        <a href="/login.html"><div class="icon_container">
+                        <a href="/login.html"><div class="icon_container pulse circle">
                             <span class="material-symbols-outlined">account_circle</span>
                             <p>Login/Signup</p>
                         </div></a>
