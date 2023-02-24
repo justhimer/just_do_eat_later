53e77ba0c67803d4f91516f580b6f4bcb1d5cc3c
Jayden Choi <henrychoi1997@hotmail.com>
2023-02-24 14:27:12 +0800

    seed final

---
+++ seeds/008-create food_types.ts
@@ -0,0 +1,87 @@
+import { Knex } from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes All existing ebtries
+    await knex("food_types").del();
+
+    await knex("food_types").insert([
+        {
+            name: "MEATBALLS WITH SPANAKOPITA RICE"
+        },
+        {
+            name: "BEEF MEX LOADED & SWEET POTATO"
+        },
+        {
+            name: "CREAMY CHICKEN KORMA"
+        },
+        {
+            name: "CHILLI CON CARNE"
+        },
+        {
+            name: "PORTUGUESE CHICKEN WITH BROCCOLI RICE"
+        },
+        {
+            name: "CHIPOTLE CHICKEN BURRITO BOWL"
+        },
+        {
+            name: "MANGO CHICKEN & BASMATI RICE"
+        },
+        {
+            name: "DELUXE CHICKEN & QUINOA BROWN RICE"
+        },
+        {
+            name: "CRUMBED CHICKEN PARMIGIANA"
+        },
+        {
+            name: "BUTTER CHICKEN & CAULIFLOWER RICE"
+        },
+        {
+            name: "MOROCCAN CHICKEN & PUMPKIN PUREE"
+        },
+        {
+            name: "HONEY SOY CHICKEN STIR FRY"
+        },
+        {
+            name: "THAI GREEN CURRY WITH CAULIFLOWER RICE"
+        },
+        {
+            name: "BUTTER CHICKEN & BASMATI RICE"
+        },
+        {
+            name: "BLACK PEPPER BEEF, PICKLED CABBAGE & VEGGIE FRIED RICE"
+        },
+        {
+            name: "MOROCCAN MEATBALLS & COUS COUS"
+        },
+        {
+            name: "BEEF BOLOGNESE & PASTA"
+        },
+        {
+            name: "BEEF HOKKIEN NOODLES"
+        },
+        {
+            name: "PEPPERCORN STEAK & CHIPS"
+        },
+        {
+            name: "THYME LEMON CHICKEN & PUMPKIN PUREE"
+        },
+        {
+            name: "MONGOLIAN BEEF WITH BROWN RICE"
+        },
+        {
+            name: "ASIAN CHICKEN STIR FRY & HOKKIEN NOODLES"
+        },
+        {
+            name: "CHICKEN & CHORIZO PAELLA"
+        },
+        {
+            name: "PERI PERI CHICKEN & VEG"
+        },
+        {
+            name: "TURKEY MINCE POKE BOWL"
+        },
+        {
+            name: "BBQ GRILLED CHICKEN & SWEET POTATO"
+        },
+    ])
+}
---
+++ seeds/011-create locations.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes All existing ebtries
+    await knex("locations").del();
+
+    await knex("locations").insert([
+        {
+            address: "15/F Lee Theatre, 99 Percival St, Causeway Bay",
+            point: "22.278299, 114.183107",
+            description: "位於利舞臺，嶄新設計令人耳目一新，頂尖器材一應俱全，更設有所有分店中面積最大的拳擊訓練專區。Pure Fitness每星期提供60多個課堂逾20項健身班選擇，由私人健身教練為顧客度身設計鍛鍊計劃，並免費提供Wi-Fi、運動服及沐浴用品等服務。",
+        },
+        {
+            address: "18/F California Tower 32 D'Aguilar Street, Central",
+            point: "22.280926, 114.155475",
+            description: "nood food juice bar serving superfood smoothies, cold-pressed juices and raw food fix"
+        },
+        {
+            address: "Shop 611 & Shop 711, Level 6 & Level 7, K11 MUSEA Victoria Dockside, 18 Salisbury Rd, Tsim Sha Tsui",
+            point: "22.293437, 114.174040",
+            description: "The biggest PURE Fitness in the world"
+        }
+    ])
+
+}
